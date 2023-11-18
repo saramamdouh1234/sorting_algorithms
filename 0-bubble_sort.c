15f30d1122eeef6b490a4ab5d22141300b444ca4
sarah mamdouh <sarah.m.alawady@gmail.com>
2023-11-18 15:10:01 -0800

    new

---
+++ 0-bubble_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+/**
+ *bubble_sort - Sort a set of integers in ascending order
+ *@array: An array of ints
+ *@size: The size of array
+ *Return: void.
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int temp;
+	size_t a, z;
+
+	if (array == NULL || size == 0)
+		return;
+
+	for (a = 0; a < size; a++)
+	{
+		z = 0;
+		while (z < size - 1)
+		{
+			if (array[z] > array[z + 1])
+			{
+				temp = array[z];
+				array[z] = array[z + 1];
+				array[z + 1] = temp;
+				print_array(array, size);
+			}
+			z++;
+		}
+	}
+}
+
