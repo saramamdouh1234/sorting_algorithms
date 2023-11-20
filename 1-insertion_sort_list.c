c628d3ac144c3a97e85cc51330bf899b87d5b1ce
sarah mamdouh <sarah.m.alawady@gmail.com>
2023-11-20 10:45:10 -0800

    new

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,47 +1,49 @@
 #include "sort.h"
 
 /**
- *swap_list - Swap 2 nodes in a listint_t.
- *@list: address to the head of the doubly-linked list.
- *@num1: the first node to swap.
- *@num2: address of the second node to swap.
+ *swap - Swap 2 nodes in a listint_t doubly-linked list.
+ *@hug: address to the head of the doubly-linked list.
+ *@num1: address to the first node to swap.
+ *@num2: second node to swap.
  */
-void swap_list(listint_t **list, listint_t *num1, listint_t **num2)
+void swap(listint_t **hug, listint_t **num1, listint_t **num2)
 {
-	(num1)->next = (*num2)->next;
+	(*num1)->next = (*num2)->next;
 	if ((*num2)->next != NULL)
-		(*num2)->next->prev = num1;
-	(*num2)->next = num1;
-	(*num2)->prev = (num1)->prev;
-	if ((num1)->prev != NULL)
-		(num1)->prev->next = (*num2);
+		(*num2)->next->prev = *num1;
+	(*num2)->prev = (*num1)->prev;
+	(*num2)->next = *num1;
+	if ((*num1)->prev != NULL)
+		(*num1)->prev->next = (*num2);
 	else
-		*list = (*num2);
-	(num1)->prev = (*num2);
-	num1 = (*num2)->prev;
+		*
+		hug = (*num2);
+	(*num1)->prev = (*num2);
+	*num1 = (*num2)->prev;
 }
 
 /**
  *insertion_sort_list - Insert sorts a doubly linked list
  *@list: Address of  the head of a doubly-linked list of integers.
+ *
+ *Description: Prints the list after each swap.
  *Return: void
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *a, *b, *c;
+	listint_t *m, *z, *a;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
-	a = (*list)->next;
-	while (a != NULL)
+
+	for (m = (*list)->next; m != NULL; m = a)
 	{
-		c = a->next;
-		b = a->prev;
-		while (b != NULL && b->n > a->n)
+		a = m->next;
+		z = m->prev;
+		while (z != NULL && m->n < z->n)
 		{
-			swap_list(list, a, &b);
+			swap(list, &z, &m);
 			print_list((const listint_t *) *list);
 		}
-		a = c;
 	}
 }
